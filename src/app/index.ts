afe4f2c6886562a4f0777617cfb63f34144724c3
bange-git <sebastienbange@gmail.com>
2022-10-31 18:41:56 +0100

    added api endpoint for office and test

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from 'express';
 import { router as UserRouter } from '../routes/userRoute';
 import { PartyRouter } from '../routes/partyRoute';
+import { OfficeRouter } from '../routes/officeRoute';
 
 const app: Application = express();
 
@@ -9,6 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/users', UserRouter);
 app.use('/api/parties', PartyRouter);
+app.use('/api/offices', OfficeRouter);
 
 app.use('/', (req: Request, res: Response): void => {
   res.json({ message: 'Allo! Catch-all route.' });
