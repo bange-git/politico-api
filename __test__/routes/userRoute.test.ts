ab177d25bdfa2e536892ad08f923da7eee22c7e7
bange-git <sebastienbange@gmail.com>
2022-11-01 10:45:29 +0100

    added user routes and tested endpoints

--- __test__/routes/userRoute.test.ts
+++ __test__/routes/userRoute.test.ts
@@ -6,9 +6,62 @@ describe('User routes', () => {
   test('Get all users', async () => {
     const res = await request(app).get('/api/users');
     expect(res.body).toEqual([
-      { name: 'james', age: 37 },
-      { name: 'susan', age: 87 },
-      { name: 'alfred', age: 56 },
+      {
+        id: 1,
+        firstname: 'James',
+        lastname: 'Conwell',
+        othername: 'Smith',
+        email: 'smith@gmail.com',
+        phoneNumber: '237699999',
+        passportUrl: 'http://passport.com/james',
+        isAdmin: false,
+      },
+      {
+        id: 2,
+        firstname: 'Peter',
+        lastname: 'Conwell',
+        othername: 'Smith',
+        email: 'smith@gmail.com',
+        phoneNumber: '237699999',
+        passportUrl: 'http://passport.com/james',
+        isAdmin: false,
+      },
+      {
+        id: 3,
+        firstname: 'Alfred',
+        lastname: 'Conwell',
+        othername: 'Smith',
+        email: 'smith@gmail.com',
+        phoneNumber: '237699999',
+        passportUrl: 'http://passport.com/james',
+        isAdmin: false,
+      },
+      {
+        id: 4,
+        firstname: 'Williams',
+        lastname: 'Conwell',
+        othername: 'Smith',
+        email: 'smith@gmail.com',
+        phoneNumber: '237699999',
+        passportUrl: 'http://passport.com/james',
+        isAdmin: false,
+      },
+    ]);
+  });
+
+  test('get a specific user', async () => {
+    const res = await request(app).get('/api/users/1');
+    expect(res.body.data).toEqual([
+      {
+        id: 2,
+        firstname: 'Peter',
+        lastname: 'Conwell',
+        othername: 'Smith',
+        email: 'smith@gmail.com',
+        phoneNumber: '237699999',
+        passportUrl: 'http://passport.com/james',
+        isAdmin: false,
+      },
     ]);
   });
 });
